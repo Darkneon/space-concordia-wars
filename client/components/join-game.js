204432c90de9b6c8b5a1530fbaee0663b001a27e
Robert Jakubowicz <robert@robert-jakubowicz.com>
2014-09-11 22:01:14 -0400

    Add join game logic client side

--- client/components/join-game.js
+++ client/components/join-game.js
@@ -1,14 +1,17 @@
-(function(window) {
-    
-    // Constructor
+(function(window) {        
     var JoinGame = function(elements) {                
-        var button = Dom.find(elements.button);                  
-        Dom.addListener(button, 'click', this.onClick);
-    };
-    
-    JoinGame.prototype.onClick = function() {
-        alert('join-game');
+        var $button = $(elements.button); 
+        this.$modal = $(elements.modalContent);
+        $button.on('click touchstart', this.onClick.bind(this));
     }
-    
+
+    JoinGame.prototype.onClick = function() {        
+        this.$modal.find('.content').html('<div class="card"><ul class="table-view"><li class="table-view-cell">Item 1</li></ul></div>');
+        this.$modal.find('.title').html('List of Rooms');
+        
+        $.post('/list-games', function(result) { alert(result); })
+         .fail(function() { console.error('list-games') });
+    }
+            
     window.JoinGame = JoinGame;
 })(window);
