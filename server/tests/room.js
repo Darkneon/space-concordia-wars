ba473e29096853d38e6e462cb94a2ff4198d2ee7
Robert Jakubowicz <robert@robert-jakubowicz.com>
2014-09-21 15:01:34 -0400

    Add: basic unit test example

---
+++ server/tests/room.js
@@ -0,0 +1,17 @@
+var mocha = require('mocha');
+var should = require('should');
+var request = require('request');
+
+describe('Room', function (done) {
+    describe('GET Rooms', function (done) {
+        it('should return [] when there are no rooms', function (done) {
+            request.get('http://localhost:3000/room', function(error, response, body) {
+                response.should.be.json;
+                var result = JSON.parse(body);
+                result.should.be.Array;
+                result.should.have.length(0);
+                done(error);
+            });
+        });
+    });
+});
